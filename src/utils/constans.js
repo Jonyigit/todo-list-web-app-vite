700a4d72cfdb9de17d6fac78e3b1b6dc262b0186
Jonyigit <jonyigitavazbekov@gmail.com>
2025-06-04 19:32:42 +0300

    Create utils folder

---
+++ src/utils/constans.js
@@ -0,0 +1,64 @@
+const mockData = [
+    {
+        id: 1,
+        label: "Learn React 1",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 2,
+        label: "Learn React 2",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 3,
+        label: "Learn React 3",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 4,
+        label: "Learn React 4",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 5,
+        label: "Learn React 5",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 6,
+        label: "Learn React 6",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 7,
+        label: "Learn React 7",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 8,
+        label: "Learn React 8",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 9,
+        label: "Learn React 9",
+        isChecked: false,
+        type: "active",
+    },
+    {
+        id: 10,
+        label: "Learn React 10",
+        isChecked: false,
+        type: "active",
+    },
+];
+
+export default mockData;
